68d759c45616c18e324c240baf68aebf5497bb2e
Andrey Pleskunou <apleskunov@bk.ru>
2020-10-03 15:34:23 +0300

    feat: typical array bla bla bla

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array = [0]) {
+    const t = array.reduce((min, current) => min = current < min ? min = current : min = min, 0);
+    return t;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array = [0]) {
+    const t = array.reduce((max, current) => max = current > max ? max = current : max = max, 0);
+    return t;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array = [0]) {
+    const total = array.reduce((total, current) => total = total + current, 0);
+    const totalIndexes = array.length;
+    if (isNaN(total / totalIndexes)) {
+        return 0;
+    } else {
+        return total / totalIndexes;
+    }
 }
